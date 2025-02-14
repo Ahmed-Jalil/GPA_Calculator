d9e1f03991696a4e6bfb5146c3eeb209efa452f0
Ahmed Jalil <m.ahmdjalil@gmail.com>
2025-02-14 17:39:17 +0500

    Backend Code Modification

--- app/src/main/java/com/example/gpacalculator/MainActivity.java
+++ app/src/main/java/com/example/gpacalculator/MainActivity.java
@@ -1,33 +1,3 @@
-//package com.example.gpacalculator;
-//
-//import android.os.Bundle;
-//
-//import androidx.activity.EdgeToEdge;
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.core.graphics.Insets;
-//import androidx.core.view.ViewCompat;
-//import androidx.core.view.WindowInsetsCompat;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//
-//
-//
-//        super.onCreate(savedInstanceState);
-////        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
-////        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-////            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-////            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-////            return insets;
-//
-//
-//
-//    }
-//}
-
 package com.example.gpacalculator;
 
 import android.annotation.SuppressLint;
@@ -38,6 +8,12 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
